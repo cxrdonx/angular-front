1fd00d4a4495a88937013093d6edc098e121b316
José Cardona <jose.eduardo.cardona@gmail.com>
2021-11-14 21:23:56 -0600

    a

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/projectWebApp'));
-app.get('/*', function(req,res) {
-    console.log("servidor angular corriendo");
+app.get('/portfolio', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/projectWebApp/index.html'));
 });
-console.log("servidor angular corriendo");
 app.listen(process.env.PORT || 8080);
