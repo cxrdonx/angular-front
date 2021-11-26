66e947ab43aa0b1002d182c14c2ae788b1f2375f
José Cardona <jose.eduardo.cardona@gmail.com>
2021-11-25 20:37:22 -0600

    modifies

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/projectWebApp'));
-app.get('/blog', function(req,res) {
+app.get('/blog', '/sobre-mi', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/projectWebApp/index.html'));
 });
-
 
 app.listen(process.env.PORT || 8080);
