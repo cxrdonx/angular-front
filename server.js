3dd53170f8cf19f20fc024bf54af5a72ccc112fa
José Cardona <jose.eduardo.cardona@gmail.com>
2021-12-02 22:25:41 -0600

    routing

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ app.use(express.static(__dirname + '/dist/projectWebApp'));
 app.get('/blog', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/projectWebApp/index.html'));
 });
+app.get('/all-proyects', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/projectWebApp/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
